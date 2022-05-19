6ab6226431ffb92a182685c6f16c44e832f76ed7
PCdevv <103156588+PCdevv@users.noreply.github.com>
2022-05-19 08:02:46 +0700

    Add files via upload

---
+++ bitwise_and.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+    int a = 6;
+    int b = 3;
+    int hasil;
+    
+    //menggunakan operator bitwise and
+    hasil = a & b;
+    cout << "a & b = " << hasil << endl;
+    
+    return 0;
+	
+}
